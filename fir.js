b7b23d808792ea5cf04ef8352daed061079cd06b
swalihsha <113001872+SWALIHSHA@users.noreply.github.com>
2023-10-31 21:49:33 +0530

    Add files via upload

---
+++ fir.js
@@ -0,0 +1,7 @@
+const searchBar = document.querySelector(".search-bar");
+
+searchBar.addEventListener("keyup", (event) => {
+  const searchQuery = event.target.value;
+
+  // TODO: Do something with the search query.
+});
